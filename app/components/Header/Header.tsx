318f037c90cef328d52d514cebfd44837d75c0ec
Toshiki Takakubo <219tosiki@gmail.com>
2023-10-22 23:08:42 +0900

    ヘッダーコンポーネント作成

---
+++ app/components/Header/Header.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { Navbar, NavbarContent, NavbarItem, Link } from "@nextui-org/react";
+import { SiGithub } from "react-icons/si";
+import { SiQiita } from "react-icons/si";
+import { AiFillMail } from "react-icons/ai";
+
+const Header = () => {
+  return (
+    <Navbar className="border-b-2 border-gray-300">
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarItem>
+          <Link color="foreground" href="/">
+            Home
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="/profile">
+            Profile
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Works
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Contact
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            My Link
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem>
+          <Link isExternal href="https://github.com/toshiki-git">
+            <SiGithub size={20} color="white" />
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link isExternal href="https://qiita.com/takakubo_toshiki">
+            <SiQiita size={35} color="white" />
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link isExternal href="https://qiita.com/takakubo_toshiki">
+            <AiFillMail size={20} color="white" />
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  );
+};
+
+export default Header;
