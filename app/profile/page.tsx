b2e9f662cf44928475c9b4123beb184de14ead79
Toshiki Takakubo <219tosiki@gmail.com>
2023-10-23 16:13:29 +0900

    コンポーネント呼び出し

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import Header from "../components/Header/Header";
 import BasicInfo from "../components/Profile/BasicInfo";
 import Skill from "../components/Profile/Skill";
+import Career from "../components/Profile/Career";
+import Internship from "../Internship";
+import Hackathon from "../components/Profile/Hackathon";
 
 const Profile = () => {
   return (
@@ -9,6 +12,9 @@ const Profile = () => {
       <Header />
       <BasicInfo />
       <Skill />
+      <Career />
+      <Internship />
+      <Hackathon />
     </div>
   );
 };
