b54caa620fe9d98de51bc1637551b67baf325b61
Toshiki Takakubo <219tosiki@gmail.com>
2023-10-23 21:05:27 +0900

    コンタクトページ作成

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Header from "../components/Header/Header";
+import Form from "../components/Contact/Form";
 
 const Contact = () => {
   return (
     <div>
       <Header />
-      Contact
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8 mt-4">
+        <div className="text-3xl font-bold">Contact</div>
+      </div>
+      <Form />
     </div>
   );
 };
