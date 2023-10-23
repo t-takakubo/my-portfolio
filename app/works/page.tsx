d7f64746b529074ef50b40fcc7dfb6b9fede7dc0
Toshiki Takakubo <219tosiki@gmail.com>
2023-10-23 18:51:37 +0900

    Workコンポーネント作成

--- app/works/page.tsx
+++ app/works/page.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Header from "../components/Header/Header";
+import Work from "../components/Works/Work";
 
 const Works = () => {
   return (
     <div>
       <Header />
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8 mt-4">
+        <div className="text-3xl font-bold">Works</div>
+      </div>
+      <Work />
     </div>
   );
 };
