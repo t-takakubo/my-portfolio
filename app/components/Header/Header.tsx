37ed02f0542e852850224e89018c1a4a9334aeca
Toshiki Takakubo <219tosiki@gmail.com>
2023-10-25 14:54:42 +0900

    ヘッダーのスマホ対応

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,14 +1,34 @@
 import React from "react";
-import { Navbar, NavbarContent, NavbarItem, Link } from "@nextui-org/react";
+import {
+  Navbar,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  NavbarMenu,
+  NavbarMenuItem,
+  NavbarMenuToggle,
+} from "@nextui-org/react";
 import { SiGithub } from "react-icons/si";
 import { SiQiita } from "react-icons/si";
 import { AiFillMail } from "react-icons/ai";
 import LinkDropdown from "./LinkDropdown";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const menuItems = [
+    { name: "Home", path: "/" },
+    { name: "Profile", path: "/profile" },
+    { name: "Works", path: "/works" },
+    { name: "Contact", path: "/contact" },
+  ];
   return (
-    <Navbar isBordered>
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+    <Navbar isBordered onMenuOpenChange={setIsMenuOpen}>
+      <NavbarMenuToggle
+        aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+        className="sm:hidden"
+      />
+      <NavbarContent className="hidden sm:flex gap-4">
         <NavbarItem>
           <Link color="foreground" href="/">
             Home
@@ -50,6 +70,16 @@ const Header = () => {
           </Link>
         </NavbarItem>
       </NavbarContent>
+
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={index}>
+            <Link color={"foreground"} href={item.path} size="lg">
+              {item.name}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
     </Navbar>
   );
 };
