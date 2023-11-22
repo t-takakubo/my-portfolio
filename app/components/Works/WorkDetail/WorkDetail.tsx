5daaa0fac17235dfb40aa2f22428caf2743e00b2
Toshiki Takakubo <219tosiki@gmail.com>
2023-11-22 21:45:19 +0900

    WorkDetailのコンポーネント化

---
+++ app/components/Works/WorkDetail/WorkDetail.tsx
@@ -0,0 +1,40 @@
+import { Card, CardBody, Image } from "@nextui-org/react";
+import React from "react";
+import data from "@/app/components/Works/WorkDetail/WorkDetail.json";
+
+const WorkDetail = ({ params }: { params: { id: string } }) => {
+  const filteredData = data.filter((item) => item.id === params.id);
+
+  if (filteredData.length === 0) {
+    return <div>指定されたIDの作品が見つかりません。</div>;
+  }
+
+  const work = filteredData[0];
+
+  return (
+    <div>
+      <div>
+        <div className="text-3xl mt-4 font-semibold">{work.name}</div>
+        <div>{work.description}</div>
+      </div>
+      <div className="gap-6 grid grid-cols-2 sm:grid-cols-4 mt-4">
+        {work.images.map((image, index) => (
+          <Card shadow="sm" key={index}>
+            <CardBody className="overflow-visible p-0">
+              <Image
+                shadow="sm"
+                radius="lg"
+                width="100%"
+                alt={work.name}
+                className="w-full object-cover"
+                src={image}
+              />
+            </CardBody>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default WorkDetail;
--- app/works/[id]/page.tsx
+++ app/works/[id]/page.tsx
@@ -1,36 +1,12 @@
-import { Card, CardBody, Image } from "@nextui-org/react";
 import React from "react";
-import data from "@/app/components/Works/WorkDetail/WorkDetail.json";
+import WorkDetail from "@/app/components/Works/WorkDetail/WorkDetail";
 
-const WorkDetail = ({ params }: { params: { id: string } }) => {
-  const filteredData = data.filter((item) => item.id === params.id);
-
-  if (filteredData.length === 0) {
-    return <div>指定されたIDの作品が見つかりません。</div>;
-  }
-
-  const work = filteredData[0];
-
+const OneWork = ({ params }: { params: { id: string } }) => {
   return (
     <div>
-      <div className="gap-2 grid grid-cols-2 sm:grid-cols-4 mt-4">
-        {work.images.map((image, index) => (
-          <Card shadow="sm" key={index}>
-            <CardBody className="overflow-visible p-0">
-              <Image
-                shadow="sm"
-                radius="lg"
-                width="100%"
-                alt={work.name}
-                className="w-full object-cover"
-                src={image}
-              />
-            </CardBody>
-          </Card>
-        ))}
-      </div>
+      <WorkDetail params={params} />
     </div>
   );
 };
 
-export default WorkDetail;
+export default OneWork;
